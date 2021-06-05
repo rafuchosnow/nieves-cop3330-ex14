9817548b9ba544142eceef0025859e0be8d49e28
rafuchosnow <rafuchosnow@gmail.com>
2021-06-05 17:08:31 -0400

    Initial Commit

---
+++ src/main/java/org/example/TaxCalculator.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import java.util.Scanner;
+import java.text.DecimalFormat;
+
+public class TaxCalculator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("What is the order amount? ");
+        String orderString = input.nextLine();
+        double orderNumber = Double.parseDouble(orderString);
+
+        System.out.print("What is the state? ");
+        String stateString = input.nextLine();
+
+        double tax = 0.055;
+        double taxCharge =  orderNumber * tax;
+        double total = orderNumber + taxCharge;
+        DecimalFormat dfDollar = new DecimalFormat("#.##");
+
+        String outputString = "The subtotal is $" + dfDollar.format(orderNumber) + ".\n" +
+                              "The Tax is $" + dfDollar.format(taxCharge) + ".\n" +
+                              "The total is $" + dfDollar.format(total) + ".";
+
+        if (!stateString.equals("WI")){
+            total = orderNumber;
+            outputString = "The total is $" + dfDollar.format(total) + ".";
+        }
+
+        System.out.println(outputString);
+
+    }
+}
